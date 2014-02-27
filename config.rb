bec766883e3e085c1948bd4bd3ee8abb163b012a
Lorenzo Caldara <lorenzo.caldara@gmail.com>
2014-02-27 23:57:26 +0100

    Fatte modifiche sul layout

--- config.rb
+++ config.rb
@@ -21,7 +21,7 @@ activate :blog do |blog|
   # Matcher for blog source files
   # blog.sources = "{year}-{month}-{day}-{title}.html"
   # blog.taglink = "tags/{tag}.html"
-  # blog.layout = "layout"
+  # blog.layout = "main"
   # blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
   # blog.year_link = "{year}.html"
@@ -41,6 +41,8 @@ activate :blog do |blog|
 end
 
 page "/feed.xml", layout: false
+page "pagine/*", :layout => :main
+page "blog/*", :layout => :main
 
 activate :directory_indexes
 
